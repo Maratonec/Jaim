39542fc1fc8645633b8867875279728c2bd33c10
Maratonec <adamcerny630@icloud.com>
2020-06-10 18:35:10 +0200

    Loading browser core message

--- Jaim/core.cpp
+++ Jaim/core.cpp
@@ -5,16 +5,15 @@ void load_message(int length)
 	for (int i = 0; i < length; i++) {
 		std::cout << i << ".." << std::endl << std::endl;
 	}
+
+	std::cout << "Jiam Browser core loaded\n";
 }
 
 int main()
 {
 	int length;
 	std::cin >> length;
-
 	load_message(length);
-
-	std::cout << "Jiam Browser core loaded\n";
 
 	return 0;
 }
