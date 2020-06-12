549da800adab22492a61b5cbafd3969fce11262c
Maratonec <adamcerny630@icloud.com>
2020-06-12 08:25:58 +0200

    Setting languages to main vector and calling function from main.

--- Jaim/core.cpp
+++ Jaim/core.cpp
@@ -4,53 +4,62 @@
 #include <clocale>
 #include <sstream>
 #include <vector> 
-
 using namespace std;
 
-int main()
-{
-	//load file and settings
-	ifstream file_load("Resources/info.lpf");
-	setlocale(LC_ALL, "");
 
-	if (file_load.is_open()) {
+class LPFParser {
 
-		//variables
-		string line;
-		string temp = "";
-		string number_language;
-		string number_string;
+public: 
+	vector<string> languages_main;
 
-		//get values from first line (number of languges and sentences)
-		getline(file_load, line);
-		stringstream ss(line);
-		getline(ss, number_language, '!');
-		getline(ss, number_string);
+	void Create()
+	{
+		//load file and settings
+		ifstream file_load("Resources/info.lpf");
+		setlocale(LC_ALL, "");
 
-		//create arrays
-		int temp_language = stoi(number_language);
-		int temp_string = stoi(number_string);
-		vector<vector<string> > strings(temp_string, vector<string>(temp_language));
-		vector<string>languages(temp_language);
-		vector<string>data(temp_string);
+		if (file_load.is_open()) {
 
-		//getting values (languages) from line 2
-		string temp_line = line;
-		getline(file_load, line);
-		int n = count(line.begin(), line.end(), '?');
-		n++;
-		ss.str(line);
-		ss.clear();
-		string temp_value;
-		for (int i = 0; i < n; i++) {
-			getline(ss, languages[i], '?');
-			cout << languages[i] << " ";
+			//variables
+			string line;
+			string temp = "";
+			string number_language;
+			string number_string;
+
+			//get values from first line (number of languges and sentences)
+			getline(file_load, line);
+			stringstream ss(line);
+			getline(ss, number_language, '!');
+			getline(ss, number_string);
+
+			//create arrays
+			int temp_language = stoi(number_language);
+			int temp_string = stoi(number_string);
+			vector<vector<string> > strings(temp_string, vector<string>(temp_language));
+			vector<string>languages(temp_language);
+			vector<string>data(temp_string);
+			//getting values (languages) from line 2
+			string temp_line = line;
+			getline(file_load, line);
+			int n = count(line.begin(), line.end(), '?');
+			n++;
+			ss.str(line);
+			ss.clear();
+			string temp_value;
+			for (int i = 0; i < n; i++) {
+				getline(ss, languages[i], '?');
+			}
+			copy(languages.begin(), languages.end(), back_inserter(languages_main));
+			cout << languages_main[1];
 		}
-		
+		else cout << "Error when reading file";
 
 	}
-	else cout << "Error when reading file";
+};
 
-
+int main() {
+	LPFParser lpf;
+	lpf.Create();
 	return 0;
 }
+
